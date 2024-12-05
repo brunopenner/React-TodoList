3b81e2e4c3fd2a3b16f63f9c26ff1cd717a8c5e5
Bruno Penner <116695999+brunopenner@users.noreply.github.com>
2024-12-05 10:22:57 +0800

    #687. The TodoList Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import CSSBaseline from "@mui/material/CssBaseline"
+import TodoList from './TodoList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
     <>
       <CSSBaseline />
       <h1>Todos</h1>
+      <TodoList />
     </>
   )
 }
