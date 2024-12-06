e634b24218d3882c6b87358f08d18c237b5ec4b4
Bruno Penner <116695999+brunopenner@users.noreply.github.com>
2024-12-06 10:38:07 +0800

    #690. The New Todo Form

---
+++ src/TodoForm.jsx
@@ -0,0 +1,42 @@
+import { Create } from "@mui/icons-material";
+import { IconButton, InputAdornment, ListItem, TextField } from "@mui/material";
+import { useState } from "react";
+
+export default function TodoForm({addTodo}) {
+    const [text, setText] = useState("");
+
+    const handleChange = (evt) => {
+        setText(evt.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addTodo(text);
+        sertText("");
+    }
+
+    return (
+        <ListItem>
+            <form onSubmit={handleSubmit} >
+                <TextField
+                    id="outlined-basic"
+                    label="Outlined"
+                    variant="outlined"
+                    onChange={handleChange}
+                    value={text}
+                    slotProps={{
+                        input: {
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton aria-label="create todo" edge="end" type="submit">
+                                        <Create />
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        },
+                    }}
+                />
+            </form>
+        </ListItem>
+    );
+}
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,6 +1,7 @@
 import List from '@mui/material/List';
 import { useState } from 'react';
 import TodoItem from './TodoItem';
+import TodoForm from './TodoForm';
 
 const initialTodos = [
     {id:1, text:"walk the dog", completed: false},
@@ -30,12 +31,19 @@ export default function TodoList() {
         })
     }
 
+    const addTodo = (text) => {
+        setTodos(prevTodos => {
+            return [...prevTodos, {text: text, id: 8, completed: false}];
+        })
+    }
+
     return (
         <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
             {todos.map(todo => {
                 return<TodoItem todo={todo} key={todo.id} removeTodo={() => removeTodo(todo.id)} toggle={() => toggleTodo(todo.id)} />
                 
             })}
+            <TodoForm  addTodo={addTodo}/>
         </List>
     )
 }
